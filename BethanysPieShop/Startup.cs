787b4c8cd9cd455a488152ad17e7f8172673435a
ASweilam <anhsbio@gmail.com>
2019-09-29 15:18:24 +0100

    Added MVC services as AddControllerWithViews. registering services for DI.

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,11 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //registering services for dependancy injection.
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            // add MVC method
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +31,24 @@ namespace BethanysPieShop
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            //use https
+            app.UseHttpsRedirection();
+            //use ststaic files in wwwroot
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //responsible for mapping an incoming request to an action in a controller.
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{Id?}");
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
             });
         }
     }
