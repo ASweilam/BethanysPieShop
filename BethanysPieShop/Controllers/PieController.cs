7325c6485ca3d61aaff5ebf15e5c4be2727f5a46
ASweilam <anhsbio@gmail.com>
2019-09-29 20:27:58 +0100

    Added the controllers folder and the Pie's Controller.

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        //these are local fields that are not initialsed. 
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+
+        //// GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
