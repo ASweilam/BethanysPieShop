33dfee9a7ad705334f856f6f39e63ff617c2a03c
ASweilam <anhsbio@gmail.com>
2019-10-12 18:59:17 +0100

    Added the httppost action method with model state checks and a new action method redturing a checkout complete message.

--- BethanysPieShop/Controllers/OrderController.cs
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -25,5 +25,37 @@ namespace BethanysPieShop.Controllers
         {
             return View();
         }
+
+        // Post: /<controller>/
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompeteMessage = "Thanks for your order!";
+
+            return View();
+
+        }
+
     }
 }
